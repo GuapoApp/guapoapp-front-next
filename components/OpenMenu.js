fbf74255ba22b3741f271aeeaa989e09b3ccd78f
Gerardo <gcespedeslnx@hotmail.com>
2025-01-14 12:05:08 -0600

    fix: files to deploy in vercel

---
+++ components/OpenMenu.js
@@ -0,0 +1,14 @@
+
+
+function OpenMenu() {
+    let menu = document.getElementById("menu")
+       
+       if (menu.classList.contains('hidden')){
+           menu.classList.remove('hidden')
+       } else{
+           menu.classList.add('hidden')
+       }
+       
+   }
+
+   export default OpenMenu
